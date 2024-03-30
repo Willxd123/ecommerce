3b62b8cafdf589c168fda090c377a9e328359ffe
wilder choque quispe <didier.c.q.17@gmail.com>
2024-03-30 11:20:28 -0400

    laravel 11

---
+++ app/Models/Producto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    use HasFactory;
+
+    //asignacion masiva
+    protected $fillable = [
+        'nombre',
+        'stock',
+        'dimension',
+        'descripcion',
+        'precio',
+        'imagen',
+        'subcategoria_id'
+    ];
+
+    //relacion uno a muchos inversa
+    public function subcategoria(){
+        return $this->belongsTo(Subcategoria::class);
+    }
+
+
+    //relacion uno a muchos
+    public function variantes(){
+        return $this->hasMany(Variante::class);
+    }
+
+    //relacion muchos a muchos 
+    public function opcions(){
+        return $this->belongsToMany(Opcion::class)
+                    ->withPivot('valor')
+                    ->withTimestamps();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+
+
+
+
+
+
+
+
 });
