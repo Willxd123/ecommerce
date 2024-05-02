124867e6f1f90c5de3a407cb6c7e0a0cac5cf049
wilder choque quispe <didier.c.q.17@gmail.com>
2024-05-01 23:52:10 -0400

    productos en vista principal

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Producto extends Model
 {
@@ -17,7 +18,12 @@ class Producto extends Model
         'imagen',
         'subcategoria_id',
     ];
-
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn() => Storage::url($this->imagen),
+        );
+    }
     //relacion uno a muchos inversa
     public function subcategoria(){
         return $this->belongsTo(Subcategoria::class);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,32 @@
 <x-app-layout>
-    
+    <div class="px-4 py-3 ">
+        <x-container>
+            <h1 class="text-3x1 font-bold text-gray-700 mb-4">
+                Ultimo productos
+                
+            </h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                @foreach ($productos as $producto)
+                    <article class="bg-white shadow rounded overflow-hidden">
+                         <img src="{{ $producto->image }}" class="w-full h-48 object-cover object-center"> 
+                         <div class="p-4">
+                            <h1 class="text-lg font-bold text-gray-700 line-clamp-2 min-h-[56px] mb-2">
+                                {{$producto->nombre}}
+                            </h1>
+                            <p class="text-gray-600 mb-4">
+                               Bs/ {{$producto->precio}}
+                            </p>
+
+                            <a href="" class="btn btn-blue block w-full text-center">
+                                Ver mas
+                            </a>
+                         </div>
+                    </article>
+                @endforeach
+            </div>
+        </x-container>
+        <div class="mt-16">
+            @include('layouts.particiones.app.footer')
+        </div>
+    </div>
 </x-app-layout>
